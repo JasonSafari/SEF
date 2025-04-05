d4f2c282ade28e931498c0f5a40996ad315a10e6
Jason Little <jasonsafari142@gmail.com>
2025-04-05 19:32:01 -0400

    Update classifier.cpp

--- classifier.cpp
+++ classifier.cpp
@@ -1,13 +1,13 @@
 void train() {
 
 printf("Train");
-printf("The first feature is added");
+printf("The second feature is added");
   
 }
 
 void predict() {
 
 printf("Predict");
-printf("he second feature is added");
+
   
 }
